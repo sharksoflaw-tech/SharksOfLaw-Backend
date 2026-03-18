81ddd11656c74c63cd77725940a31cd500b0c198
sharksoflaw-tech <sharksoflaw.tech@gmail.com>
2026-03-19 00:48:46 +0530

    Added join lawyer flow

--- src/payments-phonepe/phonepe.module.ts
+++ src/payments-phonepe/phonepe.module.ts
@@ -4,10 +4,12 @@ import { PhonePeController } from "./phonepe.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Consultation } from "../consultations/consultation.entity";
 import {JoinLawyerModule} from "../join-lawyer/join-lawyer.module";
+import {JoinLawyerApplicationEntity} from "../join-lawyer/join-lawyer-application.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Consultation]), forwardRef(() => JoinLawyerModule)],
+  imports: [TypeOrmModule.forFeature([JoinLawyerApplicationEntity, Consultation]), PhonePeModule,],
   controllers: [PhonePeController],
   providers: [PhonePeService],
+  exports: [PhonePeService],
 })
 export class PhonePeModule {}
