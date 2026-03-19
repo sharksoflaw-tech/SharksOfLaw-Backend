efbdd92918b5791780d5a618dac365f60b5bfbcf
sharksoflaw-tech <sharksoflaw.tech@gmail.com>
2026-03-19 17:32:19 +0530

    Updated consultation

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -9,15 +9,15 @@ export class PaymentsController {
 
     @Post('consultations/attempts')
     createConsultAttempt(@Body() dto: CreateConsultationAttemptDto) {
-        const frontendBaseUrl = process.env.FRONTEND_BASE_URL!;
-        const backendBaseUrl = process.env.BACKEND_BASE_URL!;
+        const frontendBaseUrl = process.env.FRONTEND_URL!;
+        const backendBaseUrl = process.env.BACKEND_URL!;
         return this.payments.createAttemptForConsultation(dto.consultationId, dto.amountInr, frontendBaseUrl, backendBaseUrl);
     }
 
     @Post('join-lawyer/attempts')
     createJoinLawyerAttempt(@Body() dto: CreateJoinLawyerAttemptDto) {
-        const frontendBaseUrl = process.env.FRONTEND_BASE_URL!;
-        const backendBaseUrl = process.env.BACKEND_BASE_URL!;
+        const frontendBaseUrl = process.env.FRONTEND_URL!;
+        const backendBaseUrl = process.env.BACKEND_URL!;
         return this.payments.createAttemptForJoinLawyer(dto.joinLawyerApplicationId, dto.amountInr, frontendBaseUrl, backendBaseUrl);
     }
 
